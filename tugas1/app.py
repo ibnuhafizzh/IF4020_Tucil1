74e9d01c887da69fb9c9908213a261bb1cc32b06
harithfakhiri <13519161@std.stei.itb.ac.id>
2023-02-03 04:39:03 +0700

    type only

--- tugas1/app.py
+++ tugas1/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
-# from utils import *
+from vigenere import *
 import sys
 
 from flask import Flask
 app = Flask(__name__)
+ciphered = "ya"
 
 
 @app.route('/')
@@ -11,13 +12,33 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/vignere', methods=['GET', 'POST'])
-def vignere():
-    return render_template('vignere.html')
+@app.route('/vigenere', methods=['GET', 'POST'])
+def vigenere():
+    if (request.method == 'GET'):
+        global ciphered
+        if ciphered!="ya":
+            teks = "hasil enkripsi : "
+            return render_template("vigenere.html", cipher=ciphered, text = teks)
+        else:
+            teks=""
+            return render_template("vigenere.html", cipher="", text= teks)
+    
+    if (request.method == 'POST'):
+        inputs = []
+        res = request.form.to_dict()
+        
+        for key, value in res.items():
+            inputs.append(value)
+        print(inputs)
 
-@app.route('/ext_vignere', methods=['GET', 'POST'])
-def ext_vignere():
-    return render_template('ext_vignere.html')
+        # print(arr_input, file=sys.stdout)
+        ciphered = vigenere_cip(inputs[0], inputs[1], 'encrypt')
+        # print(pred, file=sys.stdout)
+    return redirect(url_for('vigenere'))
+
+@app.route('/ext_vigenere', methods=['GET', 'POST'])
+def ext_vigenere():
+    return render_template('ext_vigenere.html')
 
 @app.route('/playfair', methods=['GET', 'POST'])
 def playfair():
