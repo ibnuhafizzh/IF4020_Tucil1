c99157c53f15023f6a9e2316fcdf41926f9cc0bf
harithfakhiri <13519161@std.stei.itb.ac.id>
2023-02-03 02:56:27 +0700

    template routing

--- tugas1/app.py
+++ tugas1/app.py
@@ -1,6 +1,30 @@
+from flask import Flask, render_template, request, redirect, url_for
+# from utils import *
+import sys
+
 from flask import Flask
 app = Flask(__name__)
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello world!'
-app.run()
+def index():
+    return render_template('index.html')
+
+
+@app.route('/vignere', methods=['GET', 'POST'])
+def vignere():
+    return render_template('vignere.html')
+
+@app.route('/ext_vignere', methods=['GET', 'POST'])
+def ext_vignere():
+    return render_template('ext_vignere.html')
+
+@app.route('/playfair', methods=['GET', 'POST'])
+def playfair():
+    return render_template('playfair.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
