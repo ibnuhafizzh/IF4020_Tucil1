989b5f1d5b588fb80857fcdb5e5e355b3d7e752e
ibnuhafizh <hafizhputraa@gmail.com>
2023-02-03 20:14:35 +0700

    connect autokey & affine function to ui

--- tugas1/cipher.py
+++ tugas1/cipher.py
@@ -36,7 +36,7 @@ def extended_vigenere(text, key, mode):
     return result
 
 # varian vigenere cipher (auto-key vigenere cipher)
-def autokey_vigenere(text, key, mode):
+def autokey_vigenere_cip(text, key, mode):
     text = clean_text(text)
     if (mode == "encrypt" and len(key) < len(text)):
          key = key + text
@@ -56,19 +56,71 @@ def affine_cipher(text, m, b, mode):
             result += char
             continue
         if mode == "encrypt":
-            shifted_index = (m * char_index + b) % ALPHABET_SIZE
+            shifted_index = (int(m) * char_index + int(b)) % ALPHABET_SIZE
             result += ALPHABET[shifted_index]
         elif mode == "decrypt":
             a_inv = 0
-            for i in range(ALPHABET_SIZE):
+            for i in range(1, ALPHABET_SIZE):
                 if (m * i) % ALPHABET_SIZE == 1:
                     a_inv = i
                     break
             shifted_index = (a_inv * (char_index - b + ALPHABET_SIZE)) % ALPHABET_SIZE
-            result += ALPHABET[shifted_index]       
+            result += ALPHABET[shifted_index]
+                   
     return result
 
 # hill cipher
+import numpy as np
+def adjoint(A):
+    # Check if matrix is square
+    rows, cols = np.shape(A)
+    if rows != cols:
+        raise ValueError("Matrix is not square")
+
+    # Find the adjoint matrix
+    adjA = np.zeros((rows, cols))
+    for i in range(rows):
+        for j in range(cols):
+            adjA[i][j] = (-1)**(i+j) * np.linalg.det(np.delete(np.delete(A, i, axis=0), j, axis=1))
+    return np.transpose(adjA)
+
+def det(matrix):
+    n = len(matrix)
+    if n == 1:
+        return matrix[0][0]
+    elif n == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    else:
+        determinant = 0
+        for i in range(n):
+            sub_matrix = [[matrix[j][k] for k in range(n) if k != i] for j in range(1, n)]
+            determinant += (-1)**i * matrix[0][i] * det(sub_matrix)
+        return determinant
+
+def determinant_mod(matrix):
+    determinant = det(matrix)
+    print(determinant)
+    determinant = determinant%26
+    print(determinant)
+    return determinant
+def inverse_modulo(a, m):
+    a = a % m
+    for x in range(1, m):
+        if (a * x) % m == 1:
+            return x
+    return None
+
+def modulo_inverse_mat(matrix, mod):
+    adjoin_mat = adjoint(matrix)
+    det_mod = determinant_mod(matrix)
+    inverse_mod = inverse_modulo(det_mod,26)
+    adjoin_mat = adjoin_mat*inverse_mod
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            adjoin_mat[i][j] = adjoin_mat[i][j]%mod
+    return adjoin_mat
+
 def hill_cipher(text, key, mode):
     ALPHABET_SIZE = 26
     ALPHABET = "abcdefghijklmnopqrstuvwxyz"
@@ -91,7 +143,16 @@ def hill_cipher(text, key, mode):
                     ciphertext_block[j] += (key_matrix[j][k] * text_block[k]) % ALPHABET_SIZE
             result += "".join(ALPHABET[char % ALPHABET_SIZE] for char in ciphertext_block)
     elif mode == "decrypt":
-        result = "can't decrypt"
+        key_inv = (np.round(modulo_inverse_mat(key_matrix, 26)).astype(np.int64).tolist())
+        for i in range(0, len(text), key_dimension):
+            text_block = text[i:i + key_dimension]
+            if len(text_block) < key_dimension:
+                text_block += [0] * (key_dimension - len(text_block))
+            ciphertext_block = [0] * key_dimension
+            for j in range(key_dimension):
+                for k in range(key_dimension):
+                    ciphertext_block[j] += (key_inv[j][k] * text_block[k]) % ALPHABET_SIZE
+            result += "".join(ALPHABET[char % ALPHABET_SIZE] for char in ciphertext_block)
     return result
 
 def encrypt_playfair(key, plaintext):
