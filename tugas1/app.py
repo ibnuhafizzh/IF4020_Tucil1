e782c7804ca0c1d9bb8d1817f971f545f7b83581
harithfakhiri <13519161@std.stei.itb.ac.id>
2023-02-03 18:17:22 +0700

    udah vignere, ext vignere, playfair

--- tugas1/app.py
+++ tugas1/app.py
@@ -89,7 +89,39 @@ def ext_vigenere():
 
 @app.route('/playfair', methods=['GET', 'POST'])
 def playfair():
-    return render_template('playfair.html')
+    if (request.method == 'GET'):
+        global type_trans
+        global ciphered
+        if ciphered!="" and type_trans=="encrypt__input":
+            teks = "hasil enkripsi : "
+            return render_template("playfair.html", cipher=ciphered, text = teks)
+        elif ciphered!="" and type_trans=="decrypt__input":
+            teks = "hasil dekripsi : "
+            return render_template("playfair.html", cipher=ciphered, text = teks)
+        else:
+            teks=""
+            return render_template("playfair.html", cipher="", text= teks)
+    
+    if (request.method == 'POST'):
+        inputs = []
+        res = request.form.to_dict()
+        print(res.items(), file=sys.stderr)
+        for key, value in res.items():
+            if value!="encrypt__input" and value!="decrypt__input":
+                inputs.append(value)
+            else:
+                type_trans = value
+        
+        print(inputs, file=sys.stderr)
+        
+        print(type_trans, file=sys.stderr)
+        # print(arr_input, file=sys.stdout)
+        if type_trans=="encrypt__input":
+            ciphered = encrypt_playfair(inputs[1], inputs[0])
+        elif type_trans=="decrypt__input":
+            ciphered = decrypt_playfair(inputs[1], inputs[0])
+        # print(pred, file=sys.stdout)
+    return redirect(url_for('playfair'))
 
 @app.route('/autokey_vigenere', methods=['GET', 'POST'])
 def autokey_vigenere():
--- tugas1/cipher.py
+++ tugas1/cipher.py
@@ -94,3 +94,96 @@ def hill_cipher(text, key, mode):
         result = "can't decrypt"
     return result
 
+def encrypt_playfair(key, plaintext):
+    
+    key = key.upper()
+    plaintext = plaintext.upper()
+    # prepare the matrix
+    key = key.replace("J","I")
+    
+    plaintext = plaintext.replace("J","I")
+    matrix = playfair_matrix(key)
+    
+    # format the plaintext
+    if len(plaintext)%2==1:
+        plaintext+='Z'
+    # initialize the ciphertext
+    ciphertext = ""
+    # print(matrix)
+    # loop through the plaintext by pairs
+    for i in range(0, len(plaintext), 2):
+        a, b = plaintext[i], plaintext[i + 1]
+        # print(a)
+        # get the position of each character in the matrix
+        a_row, a_col = get_position(matrix, a)
+        b_row, b_col = get_position(matrix, b)
+        
+        # apply the rule for encryption
+        if a_row == b_row:
+            # same row
+            ciphertext += matrix[a_row][(a_col + 1) % 5] + matrix[b_row][(b_col + 1) % 5]
+        elif a_col == b_col:
+            # same column
+            ciphertext += matrix[(a_row + 1) % 5][a_col] + matrix[(b_row + 1) % 5][b_col]
+        else:
+            # different row and column
+            ciphertext += matrix[a_row][b_col] + matrix[b_row][a_col]
+        print(i, a,b, ciphertext)
+    
+    return ciphertext
+
+def decrypt_playfair(key, ciphertext):
+    # prepare the matrix
+    
+    key = key.upper()
+    ciphertext = ciphertext.upper()
+    key = key.replace("J","I")
+    
+    ciphertext = ciphertext.replace("J","I")
+    matrix = playfair_matrix(key)
+    
+    # initialize the plaintext
+    plaintext = ""
+    # loop through the ciphertext by pairs
+    for i in range(0, len(ciphertext), 2):
+        a, b = ciphertext[i], ciphertext[i + 1]
+        
+        # get the position of each character in the matrix
+        a_row, a_col = get_position(matrix, a)
+        b_row, b_col = get_position(matrix, b)
+        
+        # apply the rule for decryption
+        if a_row == b_row:
+            # same row
+            plaintext += matrix[a_row][(a_col - 1) % 5] + matrix[b_row][(b_col - 1) % 5]
+        elif a_col == b_col:
+            # same column
+            plaintext += matrix[(a_row - 1) % 5][a_col] + matrix[(b_row - 1) % 5][b_col]
+        else:
+            # different row and column
+            plaintext += matrix[a_row][b_col] + matrix[b_row][a_col]
+    
+    return plaintext
+
+
+def generate_matrix(key):
+    key = key.replace(" ", "").upper()
+    matrix = []
+    for char in key + 'ABCDEFGHIKLMNOPQRSTUVWXYZ':
+        if char not in matrix:
+            matrix.append(char)
+    return matrix
+
+def playfair_matrix(key):
+    matrix = generate_matrix(key)
+    return [matrix[i:i+5] for i in range(0, 25, 5)]
+
+def get_position(matrix, char):
+    # print(matrix)
+    for i in range(5):
+        for j in range(5):
+            # print(matrix[i][j], "==", char)
+            if matrix[i][j] == char:
+                # print(i,j)
+                return (i, j)
+    return None
