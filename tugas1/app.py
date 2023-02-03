974a3a35cf06fa42fa7b99275270ea2276527268
harithfakhiri <13519161@std.stei.itb.ac.id>
2023-02-03 17:30:28 +0700

    ext sama normal vigenere berhasil

--- tugas1/app.py
+++ tugas1/app.py
@@ -13,12 +13,17 @@ def index():
     return render_template('index.html')
 
 
+
 @app.route('/vigenere', methods=['GET', 'POST'])
 def vigenere():
     if (request.method == 'GET'):
+        global type_trans
         global ciphered
-        if ciphered!="":
+        if ciphered!="" and type_trans=="encrypt__input":
             teks = "hasil enkripsi : "
+            return render_template("vigenere.html", cipher=ciphered, text = teks)
+        elif ciphered!="" and type_trans=="decrypt__input":
+            teks = "hasil dekripsi : "
             return render_template("vigenere.html", cipher=ciphered, text = teks)
         else:
             teks=""
@@ -27,22 +32,34 @@ def vigenere():
     if (request.method == 'POST'):
         inputs = []
         res = request.form.to_dict()
-        
+        print(res.items(), file=sys.stderr)
         for key, value in res.items():
-            inputs.append(value)
-        print(inputs)
-
+            if value!="encrypt__input" and value!="decrypt__input":
+                inputs.append(value)
+            else:
+                type_trans = value
+        
+        print(inputs, file=sys.stderr)
+        
+        print(type_trans, file=sys.stderr)
         # print(arr_input, file=sys.stdout)
-        ciphered = vigenere_cip(inputs[0], inputs[1], 'encrypt')
+        if type_trans=="encrypt__input":
+            ciphered = vigenere_cip(inputs[0], inputs[1], 'encrypt')
+        elif type_trans=="decrypt__input":
+            ciphered = vigenere_cip(inputs[0], inputs[1], 'decrypt')
         # print(pred, file=sys.stdout)
     return redirect(url_for('vigenere'))
 
 @app.route('/ext_vigenere', methods=['GET', 'POST'])
 def ext_vigenere():
     if (request.method == 'GET'):
+        global type_trans
         global ciphered
-        if ciphered!="":
+        if ciphered!="" and type_trans=="encrypt__input":
             teks = "hasil enkripsi : "
+            return render_template("ext_vigenere.html", cipher=ciphered, text = teks)
+        elif ciphered!="" and type_trans=="decrypt__input":
+            teks = "hasil dekripsi : "
             return render_template("ext_vigenere.html", cipher=ciphered, text = teks)
         else:
             teks=""
@@ -51,15 +68,24 @@ def ext_vigenere():
     if (request.method == 'POST'):
         inputs = []
         res = request.form.to_dict()
-        
+        print(res.items(), file=sys.stderr)
         for key, value in res.items():
-            inputs.append(value)
-        print(inputs)
-
+            if value!="encrypt__input" and value!="decrypt__input":
+                inputs.append(value)
+            else:
+                type_trans = value
+        
+        print(inputs, file=sys.stderr)
+        
+        print(type_trans, file=sys.stderr)
         # print(arr_input, file=sys.stdout)
-        ciphered = extended_vigenere(inputs[0], inputs[1], 'encrypt')
+        if type_trans=="encrypt__input":
+            ciphered = extended_vigenere(inputs[0], inputs[1], 'encrypt')
+        elif type_trans=="decrypt__input":
+            ciphered = extended_vigenere(inputs[0], inputs[1], 'decrypt')
         # print(pred, file=sys.stdout)
     return redirect(url_for('ext_vigenere'))
+
 
 @app.route('/playfair', methods=['GET', 'POST'])
 def playfair():
